12b20ef2697b2fcb48af42a41b44e9a49eb56d3e
Banny Potter <jrbancroft@googlemail.com>
2024-04-02 18:52:43 +0100

    fix powershell

--- deployment/app-deployment.ps1
+++ deployment/app-deployment.ps1
@@ -6,7 +6,7 @@ param(
 Write-Output "This is powershell executing as part of the deployment = $exampleParam"
 Write-Output "The environment user is $Env:UserName"
 
-$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -AccountName $storageAccountName)[0].Value
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName)[0].Value
 $storageContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
 $containerName = "contents"
 $blobs = Get-AzStorageBlob -Context $storageContext -Container $containerName
